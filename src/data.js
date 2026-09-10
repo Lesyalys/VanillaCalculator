841cef3325b5d46fff663f738f27a93f96132b4c
Lesya <117307391+Lesyalys@users.noreply.github.com>
2026-09-10 19:44:08 +0500

    new

---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const buttons = [
+  { id: "m+", value: "M+", type: "memory", classStyle: "calculate_gray" },
+  { id: "m-", value: "M-", type: "memory", classStyle: "calculate_gray" },
+  { id: "mr", value: "MR", type: "memory", classStyle: "calculate_gray" },
+  { id: "ms", value: "MS", type: "memory", classStyle: "calculate_gray" },
+
+  { id: "ac", value: "AC", type: "action", classStyle: "calculate_gray" },
+  { id: "mc", value: "MC", type: "memory", classStyle: "calculate_gray" },
+  { id: "percent", value: "%", type: "action", classStyle: "calculate_gray" },
+  { id: "div", value: "÷", type: "operator", classStyle: "calculate_orange" },
+
+  { id: "7", value: "7", type: "number", classStyle: "calculate_black" },
+  { id: "8", value: "8", type: "number", classStyle: "calculate_black" },
+  { id: "9", value: "9", type: "number", classStyle: "calculate_black" },
+  { id: "mul", value: "×", type: "operator", classStyle: "calculate_orange" },
+
+  { id: "4", value: "4", type: "number", classStyle: "calculate_black" },
+  { id: "5", value: "5", type: "number", classStyle: "calculate_black" },
+  { id: "6", value: "6", type: "number", classStyle: "calculate_black" },
+  { id: "sub", value: "−", type: "operator", classStyle: "calculate_orange" },
+
+  { id: "1", value: "1", type: "number", classStyle: "calculate_black" },
+  { id: "2", value: "2", type: "number", classStyle: "calculate_black" },
+  { id: "3", value: "3", type: "number", classStyle: "calculate_black" },
+  { id: "add", value: "+", type: "operator", classStyle: "calculate_orange" },
+
+  {
+    id: "0",
+    value: "0",
+    type: "number",
+    classStyle: "calculate_black calculate_zero",
+  },
+  { id: "comma", value: ",", type: "number", classStyle: "calculate_black" },
+  {
+    id: "equals",
+    value: "=",
+    type: "operator",
+    classStyle: "calculate_orange",
+  },
+];
